85840d40682ee3f9e9c05f79c4ef34a9123eb149
Michael <gblkk@yandex.ru>
2016-09-26 08:50:54 +0000

    update build.sql

--- src/vpiska/build.sql
+++ src/vpiska/build.sql
@@ -1,4 +1,4 @@
-/* task #1 */
+/* task №1 */
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
   name    VARCHAR(35)  NOT NULL,
@@ -24,18 +24,22 @@ CREATE TABLE houses (
   country_name  VARCHAR(52) REFERENCES country_to_fee (name) ON DELETE CASCADE,
   address_extra VARCHAR(100) NOT NULL,
   house_name    VARCHAR(52)  NOT NULL,
-  rooms_number  SMALLINT     NOT NULL,
-  beds_number   SMALLINT     NOT NULL,
-  max_residents SMALLINT     NOT NULL,
+  rooms_number  SMALLINT     NOT NULL CHECK (rooms_number >= 0),
+  beds_number   SMALLINT     NOT NULL CHECK (beds_number >= 0),
+  max_residents SMALLINT     NOT NULL CHECK (beds_number >= 0),
   gps_longitude FLOAT        NOT NULL,
-  gps_latitude  FLOAT        NOT NULL
+  gps_latitude  FLOAT        NOT NULL,
+  CHECK (gps_latitude >= -90 AND gps_latitude <= 90),
+  CHECK (gps_longitude >= -180 AND gps_longitude <= 180)
 );
 
 CREATE TABLE houses_gps (
   gps_longitude FLOAT NOT NULL,
   gps_latitude  FLOAT NOT NULL,
-  PRIMARY KEY (gps_longitude, gps_latitude),
-  house_id      SERIAL REFERENCES houses (house_id) ON DELETE CASCADE
+  house_id      SERIAL REFERENCES houses (house_id) ON DELETE CASCADE,
+  PRIMARY KEY (gps_longitude, gps_latitude, house_id),
+  CHECK (gps_latitude >= -90 AND gps_latitude <= 90),
+  CHECK (gps_longitude >= -180 AND gps_longitude <= 180)
 );
 
 CREATE TABLE houses_extra (
@@ -49,20 +53,34 @@ CREATE TABLE houses_extra (
 
 CREATE TABLE houses_prices (
   house_id       SERIAL REFERENCES houses (house_id) ON DELETE CASCADE,
-  prices         MONEY [53],
+  prices         MONEY [53] NOT NULL,
   cleaning_price MONEY
 );
 
 CREATE TABLE applications (
-  application_id SERIAL PRIMARY KEY,
-  date_begin     DATE NOT NULL,
-  date_end       DATE NOT NULL,
-  house_id       SERIAL REFERENCES houses (house_id) ON DELETE CASCADE,
-  user_id        SERIAL REFERENCES users (user_id) ON DELETE CASCADE,
-  commentary     VARCHAR(1000)
+  application_id   SERIAL PRIMARY KEY,
+  date_begin       DATE    NOT NULL,
+  date_end         DATE    NOT NULL,
+  residents_number INTEGER NOT NULL CHECK (residents_number >= 0),
+  commentary       VARCHAR(1000),
+  is_accepted      BIT,
+  house_id         SERIAL REFERENCES houses (house_id) ON DELETE CASCADE,
+  user_id          SERIAL REFERENCES users (user_id) ON DELETE CASCADE
 );
 
-/* task #2 */
+CREATE TABLE applications_for_user (
+  user_id        SERIAL REFERENCES users (user_id) ON DELETE CASCADE,
+  application_id SERIAL REFERENCES applications (application_id) ON DELETE CASCADE,
+  PRIMARY KEY (user_id, application_id)
+);
+
+CREATE TABLE applications_for_host (
+  house_id       SERIAL REFERENCES houses (house_id) ON DELETE CASCADE,
+  application_id SERIAL REFERENCES applications (application_id) ON DELETE CASCADE,
+  PRIMARY KEY (house_id, application_id)
+);
+
+/* task №2 */
 CREATE TABLE user_comments (
   comment_id               SERIAL,
   house_id                 SERIAL REFERENCES houses (house_id) ON DELETE CASCADE,
@@ -85,22 +103,18 @@ CREATE TABLE host_comments (
   CHECK (rate >= 1 AND rate <= 5)
 );
 
-/* task #3 */
+/* task №3 */
 CREATE TYPE genre AS ENUM ('beach', 'festival', 'sport');
 
 CREATE TABLE entertainmets (
-  entertainmet_id SERIAL PRIMARY KEY,
-  name            VARCHAR(100) NOT NULL,
+  entertainmet_id SERIAL,
   gps_longitude   FLOAT        NOT NULL,
   gps_latitude    FLOAT        NOT NULL,
+  PRIMARY KEY (gps_longitude, gps_latitude, entertainmet_id),
+  name            VARCHAR(100) NOT NULL,
   time_begin      TIMESTAMP    NOT NULL,
   time_end        TIMESTAMP    NOT NULL,
-  genre           genre        NOT NULL
-);
-
-CREATE TABLE entertainmets_gps (
-  gps_longitude   FLOAT NOT NULL,
-  gps_latitude    FLOAT NOT NULL,
-  PRIMARY KEY (gps_longitude, gps_latitude),
-  entertainmet_id SERIAL REFERENCES entertainmets (entertainmet_id) ON DELETE CASCADE
+  genre           genre        NOT NULL,
+  CHECK (gps_latitude >= -90 AND gps_latitude <= 90),
+  CHECK (gps_longitude >= -180 AND gps_longitude <= 180)
 );
